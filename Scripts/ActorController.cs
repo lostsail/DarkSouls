9e7422ab9f839f0d440b40f7fd8c3a68af8c6a0a
lostsail <980360085@qq.com>
2021-07-02 23:58:07 +0800

    scripts save

---
+++ Scripts/ActorController.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    enum State
+    {
+        ground,
+        jump,
+        fall,
+        roll,
+        jab
+    }
+
+    [SerializeField] GameObject model;
+    [SerializeField] GameObject cam;
+    PlayerInput pi;
+    Animator anim;
+    Rigidbody rb;
+    
+    State state = new State();
+
+    [SerializeField] float walkFactor;
+    [SerializeField] float runFactor;
+    [SerializeField] float turnFactor;
+    [SerializeField] Vector3 jumpThrust;
+    [SerializeField] float rollMultipliar;
+    [SerializeField] float jabMultipliar;
+
+    [SerializeField] Vector3 test01;
+
+    private void Awake()
+    {
+        pi = GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        state = State.ground;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        ActorMovement();
+        SetParameter();
+    }
+
+    private void SetParameter()
+    {
+        anim.SetFloat("forward", pi.Dmag);
+
+        if (pi.jump)
+            anim.SetTrigger("jump");
+    }
+
+    private void ActorMovement()
+    {
+        float factor = walkFactor * (pi.isRun ? runFactor : 1.0f);
+
+
+        if (!pi.lockPlanarMovement)
+        {
+            Vector3 forward = Vector3.ProjectOnPlane(cam.transform.forward, Vector3.up).normalized;
+            Vector3 right= Vector3.ProjectOnPlane(cam.transform.right, Vector3.up).normalized;
+            rb.velocity =right* pi.Dvec.x* factor+forward* pi.Dvec.z * factor + transform.up * rb.velocity.y;
+            test01 = rb.velocity;
+        }
+
+        if (pi.Dmag > 0.1f)
+            transform.forward = Vector3.Slerp(transform.forward, Vector3.ProjectOnPlane(rb.velocity, Vector3.up), turnFactor);
+
+        if (state == State.roll)
+        {
+            rb.velocity =transform.forward*anim.GetFloat("rollVelocity")*rollMultipliar;
+        }
+
+        if (state == State.jab)
+        {
+            rb.velocity = -transform.forward * anim.GetFloat("jabVelocity") * jabMultipliar;
+        }
+    }
+
+
+    //Receive Message
+
+    void OnJumpEnter()
+    {
+        state = State.jump;
+        pi.inputEnable = false;
+        pi.lockPlanarMovement = true;
+        rb.AddRelativeForce(jumpThrust, ForceMode.Impulse);
+    }
+
+    void OnGroundEnter()
+    {
+        state = State.ground;
+        pi.inputEnable = true;
+        pi.lockPlanarMovement = false;
+    }
+
+    void OnGroundExit()
+    {
+        pi.lockPlanarMovement = true;
+    }
+
+    void IsOnGround()
+    {
+        anim.SetBool("ground", true);
+    }
+
+    void IsNotOnGround()
+    {
+        anim.SetBool("ground", false);
+    }
+
+    void OnFallEnter()
+    {
+        state = State.fall;
+        pi.inputEnable = false;
+        pi.lockPlanarMovement = true;
+    }
+
+    void OnFallUpdate()
+    {
+        if (rb.velocity.magnitude > 5.0f)
+        {
+            anim.SetTrigger("roll");
+        }
+    }
+
+    void OnRollEnter()
+    {
+        state = State.roll;
+        rb.velocity = Vector3.zero;
+        pi.lockPlanarMovement = true;
+        pi.inputEnable = false;
+    }
+
+    void OnJabEnter()
+    {
+        state = State.jab;
+        rb.velocity = Vector3.zero;
+        pi.lockPlanarMovement = true;
+        pi.inputEnable = false;
+    }
+}
---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    PlayerInput pi;
+
+    float RotateX;
+    private void Awake()
+    {
+        pi = player.GetComponent<PlayerInput>();
+        RotateX = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = player.transform.position+Vector3.up*1.4f;
+        transform.rotation = player.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CameraRotate()
+    {
+        transform.Rotate(0, pi.cameraH * Time.deltaTime, 0, Space.World);
+        RotateX -= pi.cameraV * Time.deltaTime;
+        if (RotateX > 80.0f)
+            RotateX = 80.0f;
+        if (RotateX < -80.0f)
+            RotateX = -80.0f;
+        transform.eulerAngles = new Vector3(RotateX, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    private void FixedUpdate()
+    {
+        CameraRotate();
+        CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
+        transform.position = player.transform.position + Vector3.up * 1.4f;
+    }
+}
